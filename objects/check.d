7baa8b733bb9bcde804fa7ab89f1f42d1301c74f
CDdoma <sashe@bk.ru>
2020-08-25 01:36:44 +0300

    cd working, env working, pwd working

--- objects/check.d
+++
@@ -1,2 +0,0 @@
-objects/check.o: check.c libft/includes/libft.h \
- libft/includes/get_next_line.h
--- objects/env.d
+++
@@ -1,2 +0,0 @@
-objects/env.o: env.c libft/includes/libftprintf.h libft/includes/libft.h \
- libft/includes/get_next_line.h minishell.h
