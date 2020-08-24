df29c71e15a40d27b472190fdffd05249071b915
CDdoma <sashe@bk.ru>
2020-08-25 01:35:33 +0300

    cd working, env working, pwd working

--- free.c
+++ free.c
@@ -6,7 +6,7 @@
 /*   By: pdemocri <sashe@bk.ru>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/25 01:33:24 by pdemocri          #+#    #+#             */
-/*   Updated: 2020/08/25 01:33:25 by pdemocri         ###   ########.fr       */
+/*   Updated: 2020/08/25 01:35:29 by pdemocri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,5 @@ int	free_args(char ***args)
 		free((*args)[i++]);
 	free(*args);
 	*args = NULL;
-	return (0);
-}
-
-int	free_env(t_env	*env)
-{
-	t_env	*tmp;
-
-	while (env)
-	{
-		tmp = env;
-		env = env->next;
-		free(tmp);
-	}
 	return (0);
 }
