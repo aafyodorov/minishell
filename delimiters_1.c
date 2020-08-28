7ec975a1fdd57a3f5134d77bce8b7ec4ac533721
fgavin <fgavin@student.21-school.ru>
2020-08-28 14:49:12 +0300

    literal() not in dquotes dosen't work

--- delimiters_1.c
+++ delimiters_1.c
@@ -6,78 +6,95 @@
 /*   By: fgavin <fgavin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/22 18:43:51 by fgavin            #+#    #+#             */
-/*   Updated: 2020/08/24 23:35:47 by fgavin           ###   ########.fr       */
+/*   Updated: 2020/08/28 14:46:23 by fgavin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
 //TODO delete this func
-const char	*stub(const char *start, t_list **list)
+const char	*stub(const char *start, t_list **list, const char *params)
 {
+	params = NULL;
 	list = NULL;
 	start = NULL;
 	return (NULL);
 }
 
-const char	*got_backslash(const char *start, t_list **list)
+const char	*got_backslash(const char *start, t_list **list, const char *params)
 {
 	t_list		*node;
+	unsigned	flag;
 
-	node = create_node(start + 1, 1);
+	flag = (*params && *(start + 2) && *(start + 2) != *params) ? 1 : 0;
+	node = create_node(start + 1, 1, flag);
 	if (push_node(list, node))
 		return (NULL);
 	return (start + 2);
 }
 
-const char	*got_space(const char *start, t_list **list)
+const char	*got_space(const char *start, t_list **list, const char *params)
 {
 	t_list		*node;
+	unsigned	flag;
 
-	node = create_node(" ", 1);
-	while (*start == ' ')
+	while (*start == ' ' && *start != *params)
 		start++;
+	flag = (*params && *params != *start) ? 1 : 0;
+	node = create_node(" ", 1, flag);
 	if (push_node(list, node))
 		return (NULL);
 
 	return (start);
 }
 
-const char	*got_sing_bracket(const char *start, t_list **list)
+//Todo handle single '
+//TODO check sing quotes in double
+const char	*got_sing_quote(const char *start, t_list **list, const char *params)
 {
 	t_list		*node;
 	const char	*next;
+	unsigned	flag;
 
 	start++;
 	next = start;
 	while (*next != '\'')
+	{
+		if (*next == *params)
+			return (NULL);
 		++next;
-	node = create_node(start, next - start);
-	if (push_node(list, node))
-		return (NULL);
-	return (++next);
-}
-
-const char	*got_literal(const char *start,  t_list **list)
-{
-	const char	*next;
-	t_list		*node;
-
-	next = start;
-	while (is_delim(next) == -1)
-		next++;
-	node = create_node(start, next - start);
+	}
+	flag = (*(++next) && *params && *next != *params) ? 1 : 0;
+	node = create_node(start, next - start - 1, flag);
 	if (push_node(list, node))
 		return (NULL);
 	return (next);
 }
 
-const char	*got_unit_delimiter(const char *start,  t_list **list)
+const char	*got_literal(const char *start,  t_list **list, const char *params)
 {
+	const char	*next;
 	t_list		*node;
+	unsigned	flag;
 
-	node = create_node(start, 1);
+	next = start;
+	while (*next != *params && is_delim(next, params) == -1)
+		next++;
+	flag = (*next && *next != *params) ? 1 : 0;
+	node = create_node(start, next - start, flag);
 	if (push_node(list, node))
 		return (NULL);
-	return (start + 1);
+	return (next);
+}
+
+const char	*got_unit_delimiter(const char *start,  t_list **list, const char *params)
+{
+	t_list		*node;
+	unsigned	flag;
+
+	flag = (*(++start) && *start != *params) ? 1 : 0;
+	node = create_node(start - 1, 1, flag);
+	if (push_node(list, node))
+		return (NULL);
+	return (start);
 }
--- parser.c
+++ parser.c
@@ -6,32 +6,39 @@
 /*   By: fgavin <fgavin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/21 13:01:23 by fgavin            #+#    #+#             */
-/*   Updated: 2020/08/23 20:50:23 by fgavin           ###   ########.fr       */
+/*   Updated: 2020/08/28 14:46:44 by fgavin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+//TODO check Norm libft!!!
 
 #include "parser.h"
 
 #include <stdio.h>
 
 //TODO Check " ' at string start?
-const char		*get_next_part(const char *str, int *key, t_list **list)
+const char		*get_next_part(const char *str, int *key, t_list **list, char *eot)
 {
 	const char	*ptr;
 
+	*key = is_delim(str, eot);
+//	if (*eot && (*key == 0 || *key == 10))
+//		*key = -1;
 	ptr = NULL;
-		if ((*key = is_delim(str)) != -1)
+		if (*key != -1)
 		{
-			ptr = g_delims[*key].func(str, list);
-			return ((char *)ptr);
+			//params = set_params(*key, eot);
+			ptr = g_delims[*key].func(str, list, eot);
 		}
 		else
-			ptr = got_literal(str, list);
+			ptr = got_literal(str, list,eot);
 	return (ptr);
 }
 
+//TODO ignore some delimiters -> \ '
 //TODO write test when add_node() return 1 (check mem leaks)
 //TODO reverse list
+/*
 t_list			*parser(const char *str)
 {
 	const char	*ptr;
@@ -52,4 +59,35 @@ t_list			*parser(const char *str)
 	}
 	//reverse list!
 	return (list);
+}*/
+
+const char		*rec_parser(const char *str, t_list **list, char *eot)
+{
+	const char	*ptr;
+	int			key;
+	while (*str != *eot)
+	{
+		ptr = NULL;
+		key = -1;
+		if (!(ptr = get_next_part(str, &key, list, eot)))
+		{
+			ft_lstclear(list, free);
+			return (NULL);
+		}
+		str = ptr;
+	}
+	str += (*str == *eot && *eot) ? 1 : 0;
+	return (str);
+}
+
+t_list			*parser(const char *str)
+{
+
+	t_list		*list;
+
+	list = NULL;
+	if (!rec_parser(str, &list, ""))//temp
+		return (NULL);
+	return (list);
+	//reverse list!
 }
