d8065a68b1cb3aacf28e7a408fbd88d6189c153e
CDdoma <sashe@bk.ru>
2020-09-19 00:20:20 +0300

    restore ft_exit.c working with arguments

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -6,17 +6,46 @@
 /*   By: pdemocri <sashe@bk.ru>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 22:05:40 by pdemocri          #+#    #+#             */
-/*   Updated: 2020/09/18 05:20:30 by fgavin           ###   ########.fr       */
+/*   Updated: 2020/09/17 02:17:04 by pdemocri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include "minishell.h"
 
-int	ft_exit(char **args)
+static int	isnumber(char *str)
 {
-	if ((args = NULL))
+	int	i;
+	int	res;
+
+	if (!str || !str[0])
 		return (0);
-	super_ctrl_d();
-	return (0);
+	i = -1;
+	res = 1;
+	while (str[++i] && res)
+	{
+		if (!ft_isdigit(str[i]))
+			res = 0;
+	}
+	return (res);
+}
+
+int			ft_exit(char **args)
+{
+	(void)args;
+	if (args[0])
+	{
+		if (isnumber(args[0]) && !args[1])
+			g_exit_status = (unsigned char)ft_atoi(args[0]);
+		else if (isnumber(args[0]))
+			return (g_exit_status =
+			print_error("exit\nminishell: exit: too many arguments", 1));
+		else
+		{
+			ft_printf("exit\nminishell: exit: %s: need numeric argument\n",
+						args[0]);
+			exit(2);
+		}
+	}
+	exit(g_exit_status);
 }
